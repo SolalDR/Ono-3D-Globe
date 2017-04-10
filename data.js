a4530932b7a7a49818fba4324468f9f75e21cbf9
SolalDR <solal.dussout-revel@hotmail.fr>
2017-04-10 18:14:35 +0200

    Add data.js

---
+++ data.js
@@ -0,0 +1,205 @@
+var coord = [
+  {
+    content: {
+      name: "Toronto",
+      text : "Je m’appelle Jessica, j’ai bientôt 20 ans, et je vis à Toronto. J’ai toujours été assez réservée, et j’adorais écrire. Mais je n’osais pas montrer mes projets à qui que ce soit, de peur que les autres ne les aime pas.       J’avais à peu près 13 ans quand j’ai commencé à aller sur Facebook, qui n’était pas encore un truc de vieux à l’époque. *rire* Je ne voyais pas l'intérêt d’engager la conversation avec des gens que je ne connaissais pas dans la vraie vie, encore aujourd’hui j’évite, mais c’est sur Facebook que j’ai rencontré Lena.Lena est un peu comme moi, mais son talent est le dessin, et elle vit aux Etats-Unis.       Donc, je suis tombé par hasard sur l’un de ses dessins sur une page que je suivait. Il m’a beaucoup plu, je l’ai donc complimenté dans les commentaires. Quelques heures après, on avait commencé à discuter. D’où on venait, de nos passions, des séries qu’on aimait… dès le premier jour on s’entendait bien. Pendant des années, on a continuer à se parler, jamais de vive voix ou visuellement, mais ça ne nous dérangeait pas.       Nous aimions beaucoup co-écrire des histoires en s’envoyant chacune son tour des paragraphes dans la messagerie. On improvisait tout le temps, et Lena dessinait parfois nos personnages.       A force, on s’est beaucoup amélioré dans nos talents respectifs, et on avait beaucoup moins peur de montrer nos travaux et recevoir des critiques. *rire* Quand j’ai eu 18 ans, j’ai invité Lena à passer ses vacances chez moi, au Canada. C’était la première fois en 5 ans d’amitié qu’on se voyait, et le courant passait aussi facilement qu’à l’écrit.       Maintenant que nous sommes devenues beaucoup plus prolifiques en écriture et en art, nous avons même commencé un projet de BD ensemble ! C’est plus un projet personnel pour l’instant, mais avec un peu de chance...        Le numérique et internet représente une bonne partie de mes occupations aujourd’hui, mais tout ça s’est vraiment mis en marche le jour ou j’ai écrit ce commentaire sur Facebook.       Sans le numérique, même le vieux numérique de 2012, je n’aurais jamais rencontré Lena. Nous n’aurions jamais atteint le niveau d’écriture ou de dessin que nous avons aujourd’hui, qui pourra peut-être nous aider à gagner notre vie ! Et surtout, nous n’aurions jamais osé montrer nos créations aux autres, ou du moins pas aussi tôt.",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ],
+      voice: {
+        fileName: "canada.m4a",
+        duration: 53,
+        keyValues: [
+          [6300, 20],
+          [12000, 34],
+          [14500, 41],
+          [16000, 48],
+          [19500, 60],
+          [36000, 113]
+        ]
+      }
+    },
+    lat: 51.5085,
+    lon: -0.1257
+  },
+  {
+    content: {
+      name: "New York",
+      text : "Bonjour les gens, c'est génial",
+      data : [
+        {
+          count: 70,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        },
+        {
+          count: 30,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    lat:40.7142700,
+    lon: -74.0059700
+  },
+  {
+    content: {
+      name: "Brasilia",
+      text : "Bonjour les gens, c'est génial",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    lat: -15.7797200,
+    lon: -47.9297200
+  },
+  {
+    content: {
+      name: "Le Caire",
+      text : "Bonjour les gens, c'est génial",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    lat: 30.0626300,
+    lon: 31.2496700
+  },
+  {
+    content: {
+      name: "Melbourne",
+      text : "Bonjour les gens, c'est génial",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    lat: -37.8140000,
+    lon: 144.9633200
+  },
+  {
+    content: {
+      name: "Manchester",
+      text : "Bonjour les gens, c'est génial",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    lat: 53.4809,
+    lon: -2.2374
+  },
+  {
+    content: {
+      name: "Tokyo",
+      text : "Bonjour les gens, c'est génial",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    lat: 35.6895000,
+    lon: 139.6917100
+  },
+  {
+    content: {
+      name: "Namibie",
+      text : "Dans le domaine de la psychologie et de la sociologie, l'étude des relations interculturelles porte sur les contacts de culture. Lorsque des personnes de cultures différentes interagissent, elles vont mettre en commun pour communiquer, des éléments culturels qui leur sont propres tout comme certains qui leur sont communs, mais vont également faire appel à des apports culturels extérieurs à eux. Une sorte de « bricolage culturel » va se mettre en place leur permettant de dépasser les différences, sources d'obstacles à la communication, voire de les exploiter pour créer un nouvel espace culturel d'interaction, avec un nouveau code culturel. Il ne s'agit plus d'un pont entre les cultures, mais bien d'un mélange de différents rapports culturels. L'interculturel ne se borne pas au domaine de la communication comme le font la plupart des approches nées aux États-Unis. Il produit des effets au niveau des valeurs morales et des structures sociales, ce qui autorise à parler de société interculturelle.",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ],
+      voice: {
+        fileName: "interculturelle.wav",
+        duration: 53,
+        keyValues: [
+          [6300, 20],
+          [12000, 34],
+          [14500, 41],
+          [16000, 48],
+          [19500, 60],
+          [36000, 113]
+        ]
+      }
+    },
+    lat: -22.5594100,
+    lon: 17.0832300
+  },
+  {
+    content: {
+      name: "France",
+      text : "Moi, c'est Christine, j'ai 56 ans. J'ai perdu la vue brusquement à l'âge de 27 ans. En Août 1987, j'apprends que je suis atteinte d'une rétinite pigmentaire. En quelques jours je n'y vois plus rien. Je ne vais pas vous mentir, le choc a été violent mais il m'a fallu choisir entre devenir un poids pour ma famille (ma fille a alors 3 ans) et la société, ou bien me battre. J'ai choisis la deuxième solution : être la plus autonome possible et vivre une vie normale. Je pratique le ski de fond, le tandem et aussi l'équitation. Pour mes déplacements à l'extérieur, un chien guide me permet d'aller où je veux, quand je le veux en toute sécurité. J'ai aussi appris le braille et beaucoup d'autres choses indispensables encore. À côté de ça, j'ai à cœur d'aller témoigner dans des écoles, des collèges, des lycées, dans des entreprises aussi afin de mieux faire connaître le handicap visuel et permettre de favoriser le lien avec les personnes valides. Le non-voyant fait peur et en parler encore et encore aide à faire tomber des barrières. Atteinte de cécité visuelle, je ne lis actuellement que le braille. Mais il vient d'arriver sur le marché un formidable outil technologique, une caméra miniature qui se fixe sur une branche de lunette et retranscrit par voix de synthèse ce qu’elle voit. Celle-ci permet de prendre connaissance de son courrier, lire un livre, le journal, des affiches publicitaires ou même des panneaux d'affichage. Cet outil s’appelle MY EYE, c’est une révolution dans le monde des déficients visuels. Aujourd'hui j'ai testé MY EYE et je réalise toutes ces petites choses de votre quotidien qu'il me permettrait de faire et du confort incomparable qu'il m'apporterait, une véritable révolution. Juste un exemple, j'adore lire, or voilà : votre livre de poche en tant que voyant, qui tient dans votre poche, s'apparente à une encyclopédie de 6 à 9 gros volumes sitôt traduit en braille ! De plus, le coût d'un ouvrage en braille est prohibitif, donc je dois le réserver à l'AVH (Association Valentin Haüy) et il m'arrive par la Poste dans des valises plusieurs mois après sa parution en librairie. Indiscutablement, cette toute petite caméra serait une aide visuelle des plus précieuses pour moi, mais son coût élevé a brisé là net mes espoirs. Avec l’aide de l’opticien “Vue de rêve” nous avons mis en place un projet de financement collaboratif et grâce à la plateforme HUMAID, mon rêve est devenu réalité. Mon objectif de 2840€ à été atteint en quelques mois grâce aux dons des utilisateurs de la plateforme, et grâce aux services que HUMAID offre. La consommation collaborative à vraiment changé ma vie.",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ],
+      voice: {
+        fileName: "france.m4a",
+        duration: 53,
+        keyValues: [
+          [6300, 20],
+          [12000, 34],
+          [14500, 41],
+          [16000, 48],
+          [19500, 60],
+          [36000, 113]
+        ]
+      }
+    },
+    lat: 48.866667,
+    lon: 2.333333
+  },
+  {
+    content: {
+      name: "Philippine",
+      text : "Je m’appelle Lani et je suis des Philippine. Il y a neuf mois, une nouvelle a bouleversé ma vie, j’étais enceinte. Pour certains ce peut être une excellente nouvelle, le début d’une nouvelle vie. Mais moi j’avais peur. Peur de ne pas avoir être à la hauteur, peur de ne pas avoir toutes les connaissances pour élever cet enfant. Ma grossesse s’est très bien déroulée mais le moment tant attendu est arrivé plus vite que prévu. J’ai enfin pu rencontrer mon bébé, c’était merveilleux, mais un tas de questions restaient sans réponses… Heureusement, dans l'hôpital, l’organisation ACCESS Santé Internationale a mis en place le projet e-Akap. Ainsi, lors de mon séjour, j’ai eu accès à une tablette avec toutes sortes d’informations médicales et de soins concernant mon bébé. Les médecins m’ont alors expliqué que ces tablettes étaient initialement prévues pour les équipes médicales. Mais au vu du taux de la mortalité infantile, ils ont préférés les mettre à disposition des jeunes parents, novices dans l’accueil d’un bébé. Cette prévention sur les soins nécessaires aux nourrissons m’a permis, ainsi qu’à ma famille, d’être beaucoup mieux informée sur comment gérer la période post-natale et a assuré à mon bébé tous les soins et l’attention dont il avait besoin.",
+      data : [
+        {
+          count: 20,
+          type: "%",
+          text:"20% des gens sont fans du numérique"
+        }
+      ]
+    },
+    voice: {
+      fileName: "philippines.m4a",
+      duration: 53,
+      keyValues: [
+        [6300, 20],
+        [12000, 34],
+        [14500, 41],
+        [16000, 48],
+        [19500, 60],
+        [36000, 113]
+      ]
+    },
+    lat: 14.6042,
+    lon: 120.9822
+  }
+];
