4c48a4aaf4ec13f735b5734f38b17bb7ece33b6d
SolalDR <solal.dussout-revel@hotmail.fr>
2017-04-13 12:00:08 +0200

    Refacto css to scss

--- onoHystory.js
+++ onoHystory.js
@@ -164,7 +164,7 @@ SoundVolume = {
     }, false);
     this.cutSoundButton.addEventListener("click", function(){
 
-    }, false); 
+    }, false);
   },
   init:function(){
     this.mainLevelRange = document.getElementById("range-volume");
@@ -172,3 +172,51 @@ SoundVolume = {
     this.initEvents();
   }
 }
+
+
+var app = function () {
+
+  function updateSlider(element) {
+    if (element) {
+      var parent = element.parentElement,
+          lastValue = parent.getAttribute('data-slider-value');
+
+      if (lastValue === element.value) {
+        return; // No value change, no need to update then
+      }
+
+      parent.setAttribute('data-slider-value', element.value);
+      var thumb = parent.querySelector('.range-slider__thumb'),
+          bar = parent.querySelector('.range-slider__bar'),
+          pct = element.value * ((parent.clientHeight - thumb.clientHeight) / parent.clientHeight);
+
+      thumb.style.bottom = pct + '%';
+      bar.style.height = 'calc(' + pct + '% + ' + thumb.clientHeight / 2 + 'px)';
+      thumb.textContent = element.value + '%';
+    }
+  }
+  return {
+    updateSlider: updateSlider
+  };
+}();
+
+(function initAndSetupTheSliders() {
+  var inputs = [].slice.call(document.querySelectorAll('.range-slider input'));
+  inputs.forEach(function (input) {
+    return input.setAttribute('value', '50');
+  });
+  inputs.forEach(function (input) {
+    return app.updateSlider(input);
+  });
+  // Cross-browser support where value changes instantly as you drag the handle, therefore two event types.
+  inputs.forEach(function (input) {
+    return input.addEventListener('input', function (element) {
+      return app.updateSlider(input);
+    });
+  });
+  inputs.forEach(function (input) {
+    return input.addEventListener('change', function (element) {
+      return app.updateSlider(input);
+    });
+  });
+})();
